09ee35616da61194679c80537f59535be10f0e58
Ngô Hoàng Minh Trí <hoangminhtri.ngo@gmail.com>
2025-10-21 14:27:18 +0700

    21/10 14h27 done seatSelector

--- components/ui/Icon.tsx
+++ components/ui/Icon.tsx
@@ -264,3 +264,24 @@ export function StartPointIcon(props: React.SVGProps<SVGSVGElement>) {
   </svg>
   )
 }
+
+export function CircleDenseIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" className="icon icon-tabler icons-tabler-filled icon-tabler-circle-dot">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M17 3.34a10 10 0 1 1 -14.995 8.984l-.005 -.324l.005 -.324a10 10 0 0 1 14.995 -8.336zm-5 6.66a2 2 0 0 0 -1.977 1.697l-.018 .154l-.005 .149l.005 .15a2 2 0 1 0 1.995 -2.15z" />
+  </svg>
+  )
+}
+
+export function CircleThinIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+  <svg {...props} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-circle-dot">
+    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+    <path d="M12 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0" />
+    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+  </svg>
+  )
+}
+
+
